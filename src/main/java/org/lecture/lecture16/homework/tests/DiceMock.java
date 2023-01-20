6660f6404afa5ba25da6818516066bee90f07036
Shashilo Alexey <a.shashilo1998@gmail.com>
2023-01-20 11:26:24 +0700

    shaa

--- src/main/java/org/lecture/lecture16/homework/tests/DiceMock.java
+++ src/main/java/org/lecture/lecture16/homework/tests/DiceMock.java
@@ -2,22 +2,20 @@ package org.lecture.lecture16.homework.tests;
 
 import org.lecture.lecture16.homework.game.Dice;
 
-import java.util.Date;
-
 public class DiceMock implements Dice {
     private int num1;
     private int num2;
 
     private int count = 0;
 
-    DiceMock(int num1, int num2){
+    DiceMock(int num1, int num2) {
         this.num1 = num1;
         this.num2 = num2;
     }
 
     @Override
     public int roll() {
-        if (count == 0){
+        if (count == 0) {
             count++;
             return num1;
         } else {
